bdc6a16d033d9859354be59f936d98356a79b0b8
Sizimon <siizmonsus@gmail.com>
2023-01-25 14:11:30 +0000

    prog 2

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -5,11 +5,16 @@ class Category(db.Model):
     # Schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
-    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
+    tasks = db.relationship(
+        "Task",
+        backref="category",
+        cascade="all, delete",
+        lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return self.category_name
+
 
 class Task(db.Model):
     # Schema for the Task model
@@ -18,8 +23,12 @@ class Task(db.Model):
     task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+    category_id = db.Column(
+        db.Integer,
+        db.ForeignKey("category.id", ondelete="CASCADE"),
+        nullable=False
+        )
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
-        return f"#{self.id} - Task: {self.task_name} | Urgent: {self.is_urgent}"
+        return f"#{self.id} - Task: {self.task_name} | Urgent: {self.is_urgent}"  # noqa
