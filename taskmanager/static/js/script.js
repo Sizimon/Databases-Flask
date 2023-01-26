ff5ae12b881ee582c26d0c4ea2cd85e75501877c
Sizimon <siizmonsus@gmail.com>
2023-01-26 12:49:27 +0000

    Prog 4

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  let modal = document.querySelectorAll('.modal');
+  M.Modal.init(modal);
+});
